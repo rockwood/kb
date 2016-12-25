ef260555e3d73929ba4910fa8332a0c6961db0e0
Kevin Rockwood <kevin@rockwood.me>
2016-12-25 14:51:18 +0900

    Mix new

---
+++ lib/kb.ex
@@ -0,0 +1,18 @@
+defmodule Kb do
+  @moduledoc """
+  Documentation for Kb.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Kb.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kb_test.exs
@@ -0,0 +1,8 @@
+defmodule KbTest do
+  use ExUnit.Case
+  doctest Kb
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
